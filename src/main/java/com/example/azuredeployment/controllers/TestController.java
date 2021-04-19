b0eb45e66aae913b1ce85e3d266aa089243936cc
hernsys <horacioantar@gmail.com>
2021-04-19 14:48:21 -0300

    small change

--- src/main/java/com/example/azuredeployment/controllers/TestController.java
+++ src/main/java/com/example/azuredeployment/controllers/TestController.java
@@ -19,6 +19,8 @@ public class TestController {
 	@GetMapping("/helloWorld")
 	public ResponseEntity<List<String>> helloWorld() {
 		List<String> result = Arrays.asList(configText, "test 2", "test 3 updated");
+		result.add("test 4");
+		result.add("test 5");
 		return new ResponseEntity<List<String>>(result, HttpStatus.OK);
 	}
 
